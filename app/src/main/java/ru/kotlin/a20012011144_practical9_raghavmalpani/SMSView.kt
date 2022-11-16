04eecb1bff36da0085e481c953c4bbc1961166a9
rmalp <malpaniraghav65@gmail.com>
2022-11-16 16:35:50 +0500

    Initial commit

---
+++ app/src/main/java/ru/kotlin/a20012011144_practical9_raghavmalpani/SMSView.kt
@@ -0,0 +1,4 @@
+package ru.kotlin.a20012011144_practical9_raghavmalpani
+
+class SMSView(val phoneNo:String,val msg:String) {
+}
---
+++ app/src/main/java/ru/kotlin/a20012011144_practical9_raghavmalpani/SMSViewAdapter.kt
@@ -0,0 +1,23 @@
+package ru.kotlin.a20012011144_practical9_raghavmalpani
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import ru.kotlin.a20012011144_practical9_raghavmalpani.databinding.SmsItemViewBinding
+
+
+class SMSViewAdapter(context: Context, private val array:ArrayList<SMSView>):
+    ArrayAdapter<SMSView>(context,array.size,array) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        //val currentItemView:View = LayoutInflater.from(context).inflate(R.layout.sms_item_view, parent, false)
+        // get the position of the view from the ArrayAdapter
+        val currentSms: SMSView? = getItem(position)
+        val binding = SmsItemViewBinding.inflate(LayoutInflater.from(context))
+        binding.textViewPhoneNo.text = currentSms!!.phoneNo
+        binding.textViewMessage.text = currentSms.msg
+        return binding.root
+        //return super.getView(position, convertView, parent)
+    }
+}
